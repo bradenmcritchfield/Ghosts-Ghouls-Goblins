a19c6532f51ff3dba0166e56bb3ee4140faadc75
Braden Critchfield <bradenmcritchfield@gmail.com>
2023-11-08 12:58:45 -0700

    neural network

--- GGG/GhostsGhoulsGoblins.R
+++ GGG/GhostsGhoulsGoblins.R
@@ -131,3 +131,72 @@ submission <- GGG_prediction_NB %>%
   select(2, 3)
 
 vroom_write(submission, "GGGnb.csv", delim = ",")
+
+
+##########################################################
+# Neural Networks
+
+
+nn_recipe <- recipe(formula= type ~ ., data=GGGtrain) %>%
+  #update_role(id, new_role="id") %>%
+  step_mutate_at(color, fn = factor) %>%
+  step_lencode_glm(color, outcome = vars(type)) %>% ## Turn color to factor then dummy encode color
+  step_range(all_numeric_predictors(), min=0, max=1) #scale to [0,1]
+
+nn_model <- mlp(hidden_units = tune(),
+                epochs = 250, #or 100 or 250
+                activation="relu") %>%
+set_engine("keras", verbose=0) %>% #verbose = 0 prints off less
+  set_mode("classification")
+
+#for nnet
+nn_model <- mlp(hidden_units = tune(),
+                epochs = 250) %>%
+  set_engine("nnet") %>% #verbose = 0 prints off less
+  set_mode("classification")
+
+nn_wf <- workflow() %>%
+  add_recipe(nn_recipe) %>%
+  add_model(nn_model)
+
+nn_tuneGrid <- grid_regular(hidden_units(range=c(1, 100)),
+                            levels=5)
+
+folds <- vfold_cv(GGGtrain, v = 5, repeats=1)
+
+## Run the CV
+CV_results <- nn_wf %>%
+  tune_grid(resamples=folds,
+            grid=nn_tuneGrid,
+            metrics=metric_set(accuracy)) #Or leave metrics NULL
+
+CV_results %>% collect_metrics() %>%
+filter(.metric=="accuracy") %>%
+ggplot(aes(x=hidden_units, y=mean)) + geom_line()
+
+## CV tune, finalize and predict here and save results
+## This takes a few min (10 on my laptop) so run it on becker if you want
+
+
+#Find the best tuning parameters
+bestTune <- CV_results %>%
+  select_best('accuracy')
+
+final_wf <- nn_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data=GGGtrain)
+
+GGG_prediction_nn <- final_wf %>% predict(new_data = GGGtest, type="class")
+
+submission <- GGG_prediction_nn %>%
+  mutate(id = GGGtest$id) %>%
+  mutate(type = .pred_class) %>%
+  select(2, 3)
+
+vroom_write(submission, "GGGnn.csv", delim = ",")
+
+
+
+
+
+
